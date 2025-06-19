9f8378e362390ce067cd8c20781fb58d2278074b
Rolly <rollyocta0205@gmail.com>
2025-06-19 14:45:53 +0800

    update json

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // Palitan ito ng pangalan ng repository mo
 export default defineConfig({
-  base: 'rolly_app',
+  base: '/rolly_app/',
   plugins: [react()],
 })
