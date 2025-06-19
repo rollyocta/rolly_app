cfee6ae80d918192e62abffb778408b75b271414
Rolly <rollyocta0205@gmail.com>
2025-06-19 14:38:33 +0800

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Palitan ito ng pangalan ng repository mo
 export default defineConfig({
+  base: 'rolly_app',
   plugins: [react()],
 })
